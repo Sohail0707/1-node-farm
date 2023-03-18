dd9eadc180758033ad625d2909793ff2b4eca472
Sohail <sohail2001rana@gmail.com>
2023-03-18 22:58:49 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avcado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/Output.txt', textOut);
+console.log('File has been written');
+
+const uname = 'sohail';
+console.log(uname);
